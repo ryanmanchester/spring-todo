0308137cd02c0eb2aab05fb61d4515ee93dd8c35
Ryan Manchester <ryanm17@gmail.com>
2024-01-03 13:18:51 -0500

    first commit

---
+++ javatodoapp/src/main/java/com/ryanmanchester/springboot/javatodoapp/JavatodoappApplication.java
@@ -0,0 +1,13 @@
+package com.ryanmanchester.springboot.javatodoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavatodoappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavatodoappApplication.class, args);
+	}
+
+}
---
+++ javatodoapp/src/test/java/com/ryanmanchester/springboot/javatodoapp/JavatodoappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ryanmanchester.springboot.javatodoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavatodoappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
