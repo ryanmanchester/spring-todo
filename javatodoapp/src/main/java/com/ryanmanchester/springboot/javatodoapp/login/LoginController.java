e46a7e8cbd25cfcb933f13c35e0356887e58c85b
Ryan Manchester <ryanm17@gmail.com>
2024-01-04 12:16:07 -0500

    example of log4j debug logging

--- javatodoapp/src/main/java/com/ryanmanchester/springboot/javatodoapp/login/LoginController.java
+++ javatodoapp/src/main/java/com/ryanmanchester/springboot/javatodoapp/login/LoginController.java
@@ -1,5 +1,8 @@
 package com.ryanmanchester.springboot.javatodoapp.login;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,9 +11,12 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class LoginController {
 	
+	private Logger logger = LogManager.getLogger(getClass());
+	
 	@RequestMapping(path="/login")
 	public String login(@RequestParam String name, ModelMap model) {
 		model.put("name", name);
+		logger.debug("Request param is {}", name);
 		return "login";
 	}
 
