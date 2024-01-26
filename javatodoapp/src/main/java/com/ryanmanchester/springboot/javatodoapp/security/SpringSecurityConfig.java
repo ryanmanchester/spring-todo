e90c233171d83d52cea711b498c5e9cda664ff3d
Ryan Manchester <ryanm17@gmail.com>
2024-01-26 14:57:31 -0500

    adds new users

--- javatodoapp/src/main/java/com/ryanmanchester/springboot/javatodoapp/security/SpringSecurityConfig.java
+++ javatodoapp/src/main/java/com/ryanmanchester/springboot/javatodoapp/security/SpringSecurityConfig.java
@@ -15,15 +15,21 @@ public class SpringSecurityConfig {
 	
 	@Bean
 	public InMemoryUserDetailsManager createUserDetailsManager() {
+		UserDetails userDetails1 = createNewUser("Ryan", "PW01");
+		UserDetails userDetails2 = createNewUser("Cameron", "PW02");
+		
+		return new InMemoryUserDetailsManager(userDetails1, userDetails2);
+	}
+
+	private UserDetails createNewUser(String username, String password) {
 		Function<String, String> passwordEncoder = input -> passwordEncoder().encode(input);
 		UserDetails userDetails = User.builder()
 		 .passwordEncoder(passwordEncoder)
-		 .username("Ryan")
-		 .password("PW01")
+		 .username(username)
+		 .password(password)
 		 .roles("USER", "ADMIN")
 		 .build();
-		
-		return new InMemoryUserDetailsManager(userDetails);
+		return userDetails;
 	}
 	
 	@Bean
