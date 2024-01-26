ff79c33470fd99805c26eb0ac99876dc181df4c1
Ryan Manchester <ryanm17@gmail.com>
2024-01-26 13:54:17 -0500

    project cleanup to prepare for Spring security

---
+++ javatodoapp/src/main/java/com/ryanmanchester/springboot/javatodoapp/login/WelcomeController.java
@@ -0,0 +1,19 @@
+package com.ryanmanchester.springboot.javatodoapp.login;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+@Controller
+@SessionAttributes("name")
+public class WelcomeController {
+	
+	@RequestMapping(value="/", method=RequestMethod.GET)
+	public String goToWelcomePage() {
+		return "welcome";
+	}
+	
+	
+
+}
