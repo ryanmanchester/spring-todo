394dd553fd226446baf8004a7274db825f73b84b
Ryan Manchester <ryanm17@gmail.com>
2024-01-29 20:13:00 -0500

    adds more todos to h2 database

--- javatodoapp/src/main/resources/data.sql
+++ javatodoapp/src/main/resources/data.sql
@@ -1,2 +1,11 @@
 insert into todo(ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
 values(10001,'Ryan', 'Get AWS Certified', CURRENT_DATE(), false);
+
+insert into todo(ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(10002,'Ryan', 'Learn Azure', CURRENT_DATE(), false);
+
+insert into todo(ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(10003,'Ryan', 'Get Java CORE Certified', CURRENT_DATE(), false);
+
+insert into todo(ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(10004,'Ryan', 'Get DevOps Certified', CURRENT_DATE(), false);
